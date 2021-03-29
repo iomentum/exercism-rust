fba5f66da3fa094324c26c59874254de70d2a49d
yannbonaudo <yannbonaudo@yahoo.fr>
2021-03-29 14:03:32 +0200

    reforged

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,15 +1,9 @@
+
 pub fn is_leap_year(year: u64) -> bool {
-
-    if year % 4 == 0 {
-
-        if year % 100 == 0 {
-
-            if year % 400 == 0 {
-                return true;
-            } 
-        } else {
-            return true;
-        }
+    if year % 400 == 0 {
+        return true;
+    } else if year % 4 == 0 &&  year % 100 != 0 {
+        return true;
     } 
     return false;
 }

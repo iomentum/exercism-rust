5f8e03218d24f4af149e65c23dc98e9db517ed06
Rabire <rabireh@outlook.fr>
2021-03-29 11:42:22 +0200

    raindrops

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut sounds = String::new();
+
+    if n % 3 == 0 {
+        sounds.push_str("Pling")
+    }
+    
+    if n % 5 == 0 {
+        sounds.push_str("Plang")
+    } 
+    
+    if n % 7 == 0 {
+        sounds.push_str("Plong")
+    }
+
+    if sounds.is_empty() {
+        let n_str = n.to_string();
+        sounds.push_str(&n_str)
+    }
+    
+    sounds
 }

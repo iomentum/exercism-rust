5997060d53b928b427b040b6026aa40a9107c062
Yann <45734971+YannBonaudo@users.noreply.github.com>
2021-03-29 14:08:32 +0200

    Exo leap (#1)

---
+++ leap-solved/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn is_leap_year(year: u64) -> bool {
+    if year % 400 == 0 {
+        return true;
+    } else if year % 4 == 0 && year % 100 != 0 {
+        return true;
+    }
+    return false;
+}
--- leap/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
-}

0f1662322320b31572db5b2c5376440c5b25abf8
yannbonaudo <yannbonaudo@yahoo.fr>
2021-03-29 10:48:43 +0200

    up

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,16 +1,15 @@
 pub fn is_leap_year(year: u64) -> bool {
-    // if year divisible par 4
+
     if year % 4 == 0 {
-        // le chiffre est divisible par 4 et on check si il l'est par 100
+
         if year % 100 == 0 {
-            // le chiffre est divisible par 100 et on check si il l'est par 400
+
             if year % 400 == 0 {
-                // le chiffre est divisible par 100 et 400 donc ok
                 return true;
-            }
+            } 
         } else {
             return true;
         }
-    }
+    } 
     return false;
 }

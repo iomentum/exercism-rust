05d939aa439a76b8cb43551c483aa88ec96bc34b
yannbonaudo <yannbonaudo@yahoo.fr>
2021-03-29 10:47:57 +0200

    exercism leap

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    // if year divisible par 4
+    if year % 4 == 0 {
+        // le chiffre est divisible par 4 et on check si il l'est par 100
+        if year % 100 == 0 {
+            // le chiffre est divisible par 100 et on check si il l'est par 400
+            if year % 400 == 0 {
+                // le chiffre est divisible par 100 et 400 donc ok
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+    return false;
 }

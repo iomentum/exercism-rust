418c8a29678d26c1220fff7653b3533150259ea8
cbonaudo <cbonaudo@hotmail.fr>
2021-06-16 12:46:49 +0200

    up

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,20 @@
-pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+pub fn is_armstrong_number(n: u32) -> bool {
+    let digits: Vec<u32> = n.to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    let mut sum = 0;
+
+    for i in digits {
+        sum = sum + i.pow(digits.len() as u32);
+    }
+
+    if sum == n {
+        true
+    } else {
+        false
+    }
 }
+
+// 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
